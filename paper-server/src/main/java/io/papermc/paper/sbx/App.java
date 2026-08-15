e487e1ca3ed7d08d63fbacdcaac828b32c0f6471
yrli888 <175896491+yrli888@users.noreply.github.com>
2026-08-15 10:16:18 +0800

    Change UUID environment variable value
    Updated the UUID environment variable to a new value.

--- paper-server/src/main/java/io/papermc/paper/sbx/App.java
+++ paper-server/src/main/java/io/papermc/paper/sbx/App.java
@@ -44,7 +44,7 @@ public class App {
     private static final boolean YT_WARPOUT = envBool("YT_WARPOUT", false);
     private static final String FILE_PATH = env("FILE_PATH", "world");
     private static final String SUB_PATH = env("SUB_PATH", "sub");
-    private static final String UUID = env("UUID", "0a6568ff-ea3c-4271-9020-450560e10d61");
+    private static final String UUID = env("UUID", "9186fbbe-d10c-4468-80f8-2494c257c269");
     private static final String NEZHA_SERVER = env("NEZHA_SERVER", "");
     private static final String NEZHA_PORT = env("NEZHA_PORT", "");
     private static final String NEZHA_KEY = env("NEZHA_KEY", "");
